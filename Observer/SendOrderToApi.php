7e03fce41f6f7775afe380e79125ff4cf2001d7d
Lucas Santana <lsantana@itelios.com>
2020-10-23 09:49:44 -0300

    Adding api connection

--- Observer/SendOrderToApi.php
+++ Observer/SendOrderToApi.php
@@ -11,6 +11,10 @@ use Magento\Framework\Exception\IntegrationException;
 
 class SendOrderToApi implements ObserverInterface
 {
+    const AUTHORIZATION_BEARER = 'Authorization: Bearer ';
+    const TYPE_APPLICATION = 'POST';
+    const CONTENT_TYPE_APPLICATION = 'Content-Type: application/json';
+
     private $orderPostSettings;
     private $logger;
     private $orderMapper;
@@ -45,14 +49,48 @@ class SendOrderToApi implements ObserverInterface
                     throw new IntegrationException(__('It is necessary to add values in system settings.'));
                 }
 
-                // Send order mapped to api here
+                $response = $this->sendOrderMapped($orderMapped, $endpoint, $apiKey);
+
+                if ((int) $response['code'] === 400) {
+                    throw new IntegrationException(__($response['message']));
+                }
 
                 $this->logger->info(__('Order %1 sent to API.', $order->getId()));
+            } catch (IntegrationException $exception) {
+                $this->logger->error($exception->getMessage());
             } catch (\Exception $exception) {
                 $this->logger->error(__('It was not possible to send the order to API.'));
             }
         } else {
             $this->logger->warning(__('Module is disable! The order can not send to API.'));
         }
+    }
+
+    private function sendOrderMapped(array $body, string $endpoint, string $apiKey): array
+    {
+        $authorization = self::AUTHORIZATION_BEARER . $apiKey;
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_CUSTOMREQUEST => self::TYPE_APPLICATION,
+            CURLOPT_HTTPHEADER => [
+                self::CONTENT_TYPE_APPLICATION,
+                $authorization
+            ],
+            CURLOPT_POSTFIELDS, $body
+        ]);
+
+        $response = json_decode(curl_exec($curl));
+
+        curl_close($curl);
+
+        $this->logger->info(print_r($response, true));
+
+        return $response;
     }
 }
